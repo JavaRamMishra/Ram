9c13431184247fd98a15eb570ea040ca3e461129
JavaRamMishra <103430248+JavaRamMishra@users.noreply.github.com>
2022-07-26 20:18:45 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+public static void main(String[] args) {
+Student student = new Student();
+
+                  student.setId(07);
+                  student.setName("Rambo");
+                  student.setActive(true);
+           
+                            System.out.println("Student Id : "+student.getId());
+                            System.out.println("Student name : "+student.getName());
+                            System.out.println("Student active : "+student.isActive());
+                   }
+}
+
