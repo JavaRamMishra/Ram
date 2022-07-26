11810e9747ff0ebbc20e8abc2001122285f1d950
JavaRamMishra <103430248+JavaRamMishra@users.noreply.github.com>
2022-07-26 19:08:30 +0530

    Add files via upload

---
+++ Table.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Table{
+public static void main(String[] args)
+     {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Table of");
+      int a=sc.nextInt();
+      int b=a*1;
+      System.out.println(b);
+       b=a*2;
+      System.out.println(b);
+       b=a*3;
+      System.out.println(b);
+       b=a*4;
+      System.out.println(b);
+       b=a*5;
+      System.out.println(b);
+    }
+}
