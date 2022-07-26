0ea794ce35e906d6dae0c81a37cab9c8fed3c761
JavaRamMishra <103430248+JavaRamMishra@users.noreply.github.com>
2022-07-26 20:43:37 +0530

    Add files via upload

---
+++ Pyramid.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Pyramid {
+    public static void main(String[] args) {
+        System.out.println("pyramid");
+        Scanner sc =new Scanner(System.in);
+        int n = sc.nextInt();
+
+        //First loop 
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                //For Space
+                System.out.print(" ");
+            }
+            for(int k=1;k<=i;k++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+    }
+}
